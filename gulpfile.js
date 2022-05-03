46eb4d53f87e6d57cd9732a6691667491126244b
traveler20 <tabibito.traveler.sea.1039@gmail.com>
2022-05-03 19:05:56 +0900

    🆙Update files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 //gulp本体
 const gulp = require("gulp");
 
+// ejs
+const fs = require("fs");
+const ejs = require("gulp-ejs");
+const rename = require("gulp-rename");
+// prettier
+const prettier = require("gulp-prettier");
+// const csscomb = require("gulp-csscomb");
 // scss Dart Sass はSass公式が推奨 @use構文などが使える
 const sass = require("gulp-dart-sass");
 // css 縮小化
@@ -22,114 +29,137 @@ const srcBase = "./src";
 const docsBase = "./docs";
 
 const srcPath = {
-	scss: srcBase + "/asset/sass/**/*.scss",
-	js: srcBase + "/asset/js/*.js",
-	img: srcBase + "/asset/img/**",
-	html: srcBase + "/*.html",
+    scss: srcBase + "/asset/sass/**/*.scss",
+    js: srcBase + "/asset/js/*.js",
+    img: srcBase + "/asset/img/**",
+    html: srcBase + "/**/*.html",
+    ejs: [srcBase + "/**/*.ejs", "!" + srcBase + "/**/_*.ejs"],
 };
 
 const docsPath = {
-	css: docsBase + "/asset/css/",
-	js: docsBase + "/asset/js/",
-	img: docsBase + "/asset/img/",
-	html: docsBase + "/",
+    css: docsBase + "/asset/css/",
+    js: docsBase + "/asset/js/",
+    img: docsBase + "/asset/img/",
+    html: docsBase + "/",
+    ejs: docsBase + "/",
 };
 
 /**
  * sass
  */
 const cssSass = () => {
-	return gulp
-		.src(srcPath.scss, {
-			sourcemaps: true,
-		})
-		.pipe(
-			//エラーが出ても処理を止めない
-			plumber({
-				errorHandler: notify.onError("Error:<%= error.message %>"),
-			})
-		)
-		.pipe(sass({ outputStyle: "expanded" })) //指定できるキー expanded compressed
-		.pipe(
-			purgecss({
-				content: ["./src/*.html", "./src/**/*.js"], // src()のファイルで使用される可能性のあるファイルを全て指定
-			})
-		)
-		.pipe(cleancss())
-		.pipe(gulp.dest(docsPath.css, { sourcemaps: "./" })) //コンパイル先
-		.pipe(browserSync.stream())
-		.pipe(
-			notify({
-				message: "Sassをコンパイルしました！",
-				onLast: true,
-			})
-		);
+    return (
+        gulp
+            .src(srcPath.scss, {
+                sourcemaps: true,
+            })
+            .pipe(
+                //エラーが出ても処理を止めない
+                plumber({
+                    errorHandler: notify.onError("Error:<%= error.message %>"),
+                })
+            )
+            .pipe(sass({ outputStyle: "expanded" })) //指定できるキー expanded compressed
+            .pipe(
+                purgecss({
+                    content: [
+                        "./src/**/*.html",
+                        "./src/**/*.ejs",
+                        "./src/**/*.js",
+                    ], // src()のファイルで使用される可能性のあるファイルを全て指定
+                })
+            )
+            // .pipe(csscomb()) // csscombでCSSの順序指定
+            .pipe(cleancss())
+            .pipe(gulp.dest(docsPath.css, { sourcemaps: "./" })) //コンパイル先
+            .pipe(browserSync.stream())
+            .pipe(
+                notify({
+                    message: "Sassをコンパイルしました！",
+                    onLast: true,
+                })
+            )
+    );
 };
 
 /**
  * js
  */
 const js = () => {
-	return gulp
-		.src(srcPath.js)
-		.pipe(
-			//エラーが出ても処理を止めない
-			plumber({
-				errorHandler: notify.onError("Error:<%= error.message %>"),
-			})
-		)
-		.pipe(uglify())
-		.pipe(gulp.dest(docsPath.js))
-		.pipe(browserSync.stream());
+    return gulp
+        .src(srcPath.js)
+        .pipe(
+            //エラーが出ても処理を止めない
+            plumber({
+                errorHandler: notify.onError("Error:<%= error.message %>"),
+            })
+        )
+        .pipe(uglify())
+        .pipe(gulp.dest(docsPath.js))
+        .pipe(browserSync.stream());
 };
 
 /**
  * img
  */
 const img = () => {
-	return gulp
-		.src(srcPath.img)
-		.pipe(
-			//エラーが出ても処理を止めない
-			plumber({
-				errorHandler: notify.onError("Error:<%= error.message %>"),
-			})
-		)
-		.pipe(
-			imagemin([
-				imagemin.svgo(),
-				imagemin.optipng(),
-				imagemin.gifsicle({ optimizationLevel: 3 }),
-			])
-		)
-		.pipe(gulp.dest(docsPath.img))
-		.pipe(browserSync.stream());
+    return gulp
+        .src(srcPath.img)
+        .pipe(
+            //エラーが出ても処理を止めない
+            plumber({
+                errorHandler: notify.onError("Error:<%= error.message %>"),
+            })
+        )
+        .pipe(
+            imagemin([
+                imagemin.svgo(),
+                imagemin.optipng(),
+                imagemin.gifsicle({ optimizationLevel: 3 }),
+            ])
+        )
+        .pipe(gulp.dest(docsPath.img))
+        .pipe(browserSync.stream());
 };
 
 /**
  * html
  */
 const html = () => {
-	return gulp.src(srcPath.html).pipe(gulp.dest(docsPath.html));
+    return gulp.src(srcPath.html).pipe(gulp.dest(docsPath.html));
+};
+
+/**
+ * ejs
+ */
+const ejsHtml = () => {
+    const json = JSON.parse(fs.readFileSync("./ejs-config.json"));
+    return gulp
+        .src(srcPath.ejs)
+        .pipe(plumber())
+        .pipe(ejs(json, { ext: ".html" }))
+        .pipe(rename({ extname: ".html" }))
+        .pipe(prettier())
+        .pipe(gulp.dest(docsPath.ejs));
 };
 
 /**
  * ローカルサーバー立ち上げ
  */
 const browserSyncFunc = () => {
-	browserSync.init(browserSyncOption);
+    browserSync.init(browserSyncOption);
 };
 
 const browserSyncOption = {
-	server: docsBase,
+    server: docsBase,
 };
 
 /**
  * リロード
  */
 const browserSyncReload = (done) => {
-	browserSync.reload();
-	done();
+    browserSync.reload();
+    done();
 };
 
 /**
@@ -138,10 +168,11 @@ const browserSyncReload = (done) => {
  * watch('監視するファイル',処理)
  */
 const watchFiles = () => {
-	gulp.watch(srcPath.scss, gulp.series(cssSass));
-	gulp.watch(srcPath.js, gulp.series(js));
-	gulp.watch(srcPath.img, gulp.series(img));
-	gulp.watch(srcPath.html, gulp.series(html, browserSyncReload));
+    gulp.watch(srcPath.scss, gulp.series(cssSass));
+    gulp.watch(srcPath.js, gulp.series(js));
+    gulp.watch(srcPath.img, gulp.series(img));
+    gulp.watch(srcPath.html, gulp.series(html, browserSyncReload));
+    gulp.watch(srcPath.ejs, gulp.series(ejsHtml, browserSyncReload));
 };
 
 /**
@@ -149,6 +180,6 @@ const watchFiles = () => {
  * parallelは並列で実行
  */
 exports.default = gulp.series(
-	gulp.parallel(html, img, js, cssSass),
-	gulp.parallel(watchFiles, browserSyncFunc)
+    gulp.parallel(html, ejsHtml, img, js, cssSass),
+    gulp.parallel(watchFiles, browserSyncFunc)
 );
