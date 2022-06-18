62dcd2b012255f7da06242a19d5b560e1bfe7b10
traveler20 <tabibito.traveler.sea.1039@gmail.com>
2022-06-19 08:35:18 +0900

    ♻️Refactoring ...

--- src/asset/js/script.js
+++ src/asset/js/script.js
@@ -1,40 +1,95 @@
-// #js-loading animation
-window.onload = function () {
-    const loadingAnimation = document.getElementById("js-loading");
+// // #js-loading animation
+// window.onload = function () {
+//     const loadingAnimation = document.getElementById("js-loading");
 
-    loadingAnimation.classList.add("is-loaded");
+//     loadingAnimation.classList.add("is-loaded");
 
-    document.getElementById("js-fv").classList.add("is-show");
-};
+//     document.getElementById("js-fv").classList.add("is-show");
+// };
 
-// swiper
+// // swiper
+// const swiper = new Swiper(".swiper-container", {
+//     loop: true,
+//     // ページネーション
+//     pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
+//     },
+//     // 前後の矢印
+//     navigation: {
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev",
+//     },
+// });
+
+// const share_twitter = document.getElementById("js-share-twitter");
+// const share_url = document.getElementById("js-share-url");
+// const url = location.href;
+// const title = document.title;
+// share_twitter.setAttribute(
+//     "href",
+//     "https://twitter.com/share?url=" +
+//         url +
+//         "&text=" +
+//         title +
+//         "&via=kokopp_designer&hashtags=ここっぷ"
+// );
+// share_url.onclick = function () {
+//     navigator.clipboard.writeText("【" + title + "】" + " " + url);
+//     document.getElementById("js-share-url").value = "コピーしました！";
+// };
+
+// ────────────────────────────────────────────────────────────
+(window.onload = function () {
+    const e = document.getElementById("js-loading");
+    e.classList.add("is-loaded"),
+        document.getElementById("js-fv").classList.add("is-show");
+}),
+    (function () {
+        const e = document.getElementById("js-header");
+        let t = 0,
+            n,
+            i = !1;
+        window.addEventListener("scroll", function () {
+            (n = window.scrollY),
+                i ||
+                    (window.requestAnimationFrame(function () {
+                        n,
+                            60 < n &&
+                                (n > t
+                                    ? e.classList.add("is-headIn")
+                                    : e.classList.remove("is-headIn"),
+                                (t = n)),
+                            (i = !1);
+                    }),
+                    (i = !0));
+        });
+    })();
+
 const swiper = new Swiper(".swiper-container", {
-    loop: true,
-    // ページネーション
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    // 前後の矢印
+    loop: !0,
+    pagination: { el: ".swiper-pagination", clickable: !0 },
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
 });
-
-const share_twitter = document.getElementById("js-share-twitter");
-const share_url = document.getElementById("js-share-url");
-const url = location.href;
-const title = document.title;
-share_twitter.setAttribute(
-    "href",
-    "https://twitter.com/share?url=" +
-        url +
-        "&text=" +
-        title +
-        "&via=kokopp_designer&hashtags=ここっぷ"
-);
-share_url.onclick = function () {
-    navigator.clipboard.writeText("【" + title + "】" + " " + url);
-    document.getElementById("js-share-url").value = "コピーしました！";
-};
+function urlCopy() {
+    var e = location.href,
+        t = document.title;
+    navigator.clipboard.writeText("【" + t + "】 " + e),
+        (document.getElementById("urlcopyBtn").value = "コピーしました！");
+}
+function twitterShare() {
+    const e = document.getElementById("twitterBtn");
+    var t = location.href,
+        n = document.title;
+    e.setAttribute(
+        "href",
+        "https://twitter.com/share?url=" +
+            t +
+            "&text=" +
+            n +
+            "&via=kokopp_designer&hashtags=ここっぷ"
+    );
+}
